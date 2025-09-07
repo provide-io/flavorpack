2a0e8f4078fc7a0258686796693ae5fffa47db3a
Tim <code@tim.life>
2025-09-07 11:44:23 -0700

    🔼⚙️ 🌶️ [skip ci] auto-commit

--- src/flavor/packaging/python_packager.py
+++ src/flavor/packaging/python_packager.py
@@ -22,7 +22,7 @@ from flavor.psp.format_2025.constants import (
     DEFAULT_EXECUTABLE_PERMS,
 )
 from provide.foundation.platform import get_arch_name, get_os_name
-from provide.foundation.process import run_command
+from flavor.utils import run_command
 from flavor.utils.archive import deterministic_filter
 from flavor.packaging.python.pip_manager import PyPaPipManager
 
