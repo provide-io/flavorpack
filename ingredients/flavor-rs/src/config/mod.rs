287a0faf289d33f5137eb9d2b22bbd6ab335e1da
Tim <code@tim.life>
2025-09-15 16:59:24 -0700

    🔼⚙️ 🌶️ [skip ci] auto-commit

--- ingredients/flavor-rs/src/config/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Configuration module for FlavorPack
-
-pub mod defaults;
-
-pub use defaults::{
-    get_security_defaults, get_validation_defaults, SecurityDefaults, ValidationDefaults,
-    ValidationLevel,
-};
