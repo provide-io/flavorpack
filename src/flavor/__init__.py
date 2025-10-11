3693c3f188e76902c2162d6442b5e1ca8c740d30
Tim <code@tim.life>
2025-10-11 14:14:01 -0700

    🔼⚙️ 🌶️ [skip ci] auto-commit

--- src/flavor/__init__.py
+++ src/flavor/__init__.py
@@ -6,11 +6,6 @@ This package contains the core logic for building and verifying the
 Pyvider Secure Package Format (Flavor).
 """
 
-import os
-
-# Set service name for Foundation logging/telemetry (uses OTEL standard)
-os.environ.setdefault("PROVIDE_SERVICE_NAME", "flavor")
-
 from flavor._version import __version__
 from flavor.exceptions import BuildError, VerificationError
 from flavor.package import (
