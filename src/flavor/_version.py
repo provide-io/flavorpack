11004fd95adcaee2019d42a7eb0b19c7167bad4e
Tim <code@tim.life>
2025-10-19 16:05:39 -0700

    🔼⚙️ 🌶️ [skip ci] auto-commit

--- src/flavor/_version.py
+++ src/flavor/_version.py
@@ -3,12 +3,12 @@
 # SPDX-FileCopyrightText: Copyright (c) provide.io llc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-"""from provide.foundation.utils.versioning import get_version
-
 """Version handling for flavorpack.
 
 This module uses the shared versioning utility from provide-foundation.
 """
+
+from provide.foundation.utils.versioning import get_version
 
 __version__ = get_version("flavorpack", caller_file=__file__)
 
