d71db1dc1c55060254ead4e7ff2912ea283900c8
Tim <code@tim.life>
2025-10-23 20:00:00 -0700

    chore: update test configuration for conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import shutil
 import tempfile
 
 from cryptography.hazmat.primitives.asymmetric import ed25519
+import provide.testkit  # noqa: F401 - Installs setproctitle blocker early
 from provide.testkit.logger import reset_foundation_setup_for_testing
 import pytest
 
