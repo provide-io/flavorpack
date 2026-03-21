456fcfb49f9930c585f365f050cd5bc8ec6c070e
Tim <code@tim.life>
2026-03-21 15:55:01 -0700

    chore: bump Go and Rust binary versions to 0.3.21

--- src/flavor-rs/src/version.rs
+++ src/flavor-rs/src/version.rs
@@ -4,7 +4,7 @@
 //! Version information for Flavor binaries
 
 /// Current version of Flavor Rust implementation
-pub const VERSION: &str = "0.3.25";
+pub const VERSION: &str = "0.3.21";
 
 /// Build timestamp (set at compile time)
 pub const BUILD_TIME: Option<&str> = option_env!("BUILD_TIME");
