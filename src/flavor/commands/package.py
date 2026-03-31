e3983de92f04473e0f41e52cf62ade7f5e38c23d
Tim <code@tim.life>
2026-03-31 11:30:51 -0700

    refactor(python): centralize FLAVOR_* env var names as constants in env_vars.py
    All FLAVOR_* env var string literals in direct-read call sites are replaced
    with named constants from the new flavor.env_vars module.

--- src/flavor/commands/package.py
+++ src/flavor/commands/package.py
@@ -14,6 +14,7 @@ from provide.foundation.console import perr, pout
 
 from flavor.config.defaults import ENV_WORKENV_BASE
 from flavor.console import get_command_logger
+from flavor.env_vars import WORKENV_BASE
 from flavor.exceptions import BuildError, PackagingError
 from flavor.package import build_package_from_manifest, verify_package
 
@@ -145,7 +146,7 @@ def _setup_workenv_base(workenv_base: str | None) -> None:
     if workenv_base:
         import os
 
-        os.environ[ENV_WORKENV_BASE] = workenv_base
+        os.environ[WORKENV_BASE] = workenv_base
 
 
 def _build_package_artifacts(
