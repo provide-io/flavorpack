6b7a37d41379c564849052f111df7354ab8d1f9f
Tim <code@tim.life>
2026-03-30 20:15:29 -0700

    test(parity): add SBOM and attestation digest parity tests

--- tests/parity/test_sbom_parity.py
+++ tests/parity/test_sbom_parity.py
@@ -15,8 +15,6 @@ import pytest
 from flavor.psp.format_2025.attestation import build_attestation, parse_attestation
 from flavor.psp.format_2025.sbom import build_sbom
 
-pytestmark = [pytest.mark.cross_language, pytest.mark.ci, pytest.mark.security]
-
 
 def _minimal_package_info() -> dict[str, object]:
     return {
