e500ba6ac464b057737f4a7d7f0c475ca629f321
Tim <code@tim.life>
2026-04-02 14:48:31 -0700

    fix(ci): fix #!/bin/bash shebangs for FreeBSD compatibility
    FreeBSD bash is at /usr/local/bin/bash, not /bin/bash. Scripts with
    #!/bin/bash shebangs fail with exit 126 when executed directly.
    
    Fix all pretaster test scripts, slot scripts, and CI scripts that run
    inside or are called from FreeBSD VMs.
    
    (cherry picked from commit bb6be13eb3521f91b6e86e7fc262d667942339a2)

--- ci/get-version.sh
+++ ci/get-version.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# SPDX-FileCopyrightText: Copyright (c) 2026 provide.io llc. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
 # Get the current version from VERSION file
 # Usage: get-version.sh
 
