c2224803b14155fd04e897407732085e25d67687
Tim <code@tim.life>
2026-04-03 20:00:00 -0700

    fix(build): address K&R C incompatibility on macOS and FreeBSD by using -std=gnu11 and suppressing deprecated-non-prototype warnings
    Also:
    - fix(build): ensure autotools are installed on macOS by always running brew install autotools
    - scaffold(ci): introduce FreeBSD build script and integrate tastesh into FreeBSD CI jobs
    - chore(ci): update macOS and FreeBSD CI workflows to include tastesh build and cache

--- ci/build-dash.sh
+++ ci/build-dash.sh
@@ -58,11 +58,11 @@ case "$OS" in
     darwin)
         # macOS: dynamic (Apple linker doesn't support -static for executables)
         # Suppress K&R prototype errors in dash 0.5.12 with newer Xcode clang
-        CFLAGS="-Wno-error=deprecated-non-prototype" ./configure --quiet
+        CFLAGS="-std=gnu11 -Wno-deprecated-non-prototype" ./configure --quiet
         ;;
     freebsd)
-        # FreeBSD: static if possible
-        CFLAGS="-static" LDFLAGS="-static" ./configure --quiet
+        # FreeBSD: static build; use gnu11 to avoid K&R errors with newer clang
+        CFLAGS="-static -std=gnu11" LDFLAGS="-static" ./configure --quiet
         ;;
     windows)
         # Cross-compile with mingw
